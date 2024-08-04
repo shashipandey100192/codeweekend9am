40c02b15b250a4bcdad586207fba0f25d5687dd3
shashiweb <shashiweb64@gmail.com>
2024-08-04 10:28:11 +0530

    change file

---
+++ src/Contactpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contactpage() {
+  return (
+    <div>
+        <h1 className='abc'>This is contact page</h1>
+    </div>
+  )
+}
+
+export default Contactpage
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Abc, { Myabout1, Myimg, Mypage, Mypage1 } from './About';
+import "./style.css";
+import Contactpage from './Contactpage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
+    <Contactpage/>
    <Abc></Abc>
    <Myabout1></Myabout1>
    <Mypage></Mypage>
